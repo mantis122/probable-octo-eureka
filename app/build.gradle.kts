a1961ef60eb3b125da6ec6d7781aa58af5a79395
mantis122 <46855439+mantis122@users.noreply.github.com>
2026-06-07 00:22:05 -0500

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,14 +4,24 @@ plugins {
 }
 
 android {
-    namespace = "com.colorcottage.app"
+   namespace = "com.example.coloringapp"
     compileSdk = 35
 
     defaultConfig {
-        applicationId = "com.colorcottage.app"
-        minSdk = 24
+        applicationId = "com.example.coloringapp"
+        minSdk = 23
         targetSdk = 35
         versionCode = 1
         versionName = "0.1"
     }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+}
+
+
+kotlin {
+    jvmToolchain(17)
 }
